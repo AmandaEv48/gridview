0fe26d8f64ee2967f828369a03bd0956311a319c
AmandaEv48 <amanda.costa48@aluno.ce.gov.br>
2024-04-04 08:27:48 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           children: List.generate(100, (index) {
             return Center(
               child: Text(
-                'Item $index'
+                'Icon $index',
                 style: Theme.of(context).textTheme.bodyText1,
               ),
             );
